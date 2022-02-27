39f49503096ff79789cf73c04a1f774ca6135368
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-27 00:27:38 -0300

    Ao clicar no botão Enviar, o conteúdo dentro do Form principal e substituido pelas informaçoes preenchidas

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const buttonForm = document.querySelector('#submit-btn');
 const checkbox = document.querySelector('#agreement');
 const counter = document.querySelector('#counter');
 const textArea = document.querySelector('#textarea');
+const form = document.querySelector('#evaluation-form');
+const house = document.querySelector('#house');
+const family = document.querySelectorAll('input[name="family"]');
+const rate = document.querySelectorAll('input[name="rate"]');
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const subjects = document.querySelectorAll('.subject');
 buttonForm.disabled = true;
 
 function loginEnter() {
@@ -30,3 +38,56 @@ function counterCharacter() {
   counter.innerText = 500 - qtdCharacter;
 }
 textArea.addEventListener('keyup', counterCharacter);
+
+// requisito 21
+function familyValue() {
+  let stringFamily = '';
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked) {
+      stringFamily = family[index].value;
+    }
+  }
+  return stringFamily;
+}
+
+function subjectsValue() {
+  let stringSubjects = '';
+  for (let index = 0; index < subjects.length; index += 1) {
+    if (subjects[index].checked) {
+      stringSubjects += `${subjects[index].value} `;
+    }
+  }
+  return stringSubjects;
+}
+
+function rateValue() {
+  let stringRate = '';
+  for (let index = 0; index < rate.length; index += 1) {
+    if (rate[index].checked) {
+      stringRate = rate[index].value;
+    }
+  }
+  return stringRate;
+}
+
+function values() {
+  const array = [];
+  array.push(`${'Nome'}: ${inputName.value} ${inputLastName.value}`);
+  array.push(`${'Email'}: ${inputEmail.value}`);
+  array.push(`${'Casa'}: ${house.value}`);
+  array.push(`${'Família'}: ${familyValue()}`);
+  array.push(`${'Matérias'}: ${subjectsValue()}`); // No navegador o retorno está correto, mas não passa no teste.
+  array.push(`${'Avaliação'}: ${rateValue()}`);
+  array.push(`${'Observações'}: ${textArea.value}`);
+  return array;
+}
+
+function createParagraph() {
+  form.innerHTML = '';
+  for (let index = 0; index < values().length; index += 1) {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = values()[index];
+    form.appendChild(paragraph);
+  }
+}
+buttonForm.addEventListener('click', createParagraph);
